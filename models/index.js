9596cebebeecb7007e0b288f5598c636d11b3242
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-09-14 15:33:17 -0700

    Updated schema.js file

--- models/index.js
+++ models/index.js
@@ -5,27 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 
 });
 
 // Categories have many Products
-Category.hasMany(Products, {
+Category.hasMany(Product, {
   foreignKey: 'product_id',
   onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tags, {
+Product.belongsToMany(Tag, {
   foreignKey: 'product_id',
-  through:'productTag',
+  through:ProductTag,
 });
 // Tags belongToMany Products (through ProductTag)
 
-Tag.belongsToMany(Products, {
+Tag.belongsToMany(Product, {
   foreignKey: 'tag_id',
-  through:'productTag',
+  through:ProductTag,
 })
 module.exports = {
   Product,
